fdb36b63890689c42842d516b546f92f1a23de26
bipplus1 <markx195>
2023-05-15 15:55:36 +0700

    draft js still

--- src/RichText/Draft.js
+++ src/RichText/Draft.js
@@ -1,52 +1,138 @@
-import React from "react";
-import { DraftailEditor } from "draftail";
-import { EditorState } from "draft-js";
-import createInlineToolbarPlugin from "draft-js-inline-toolbar-plugin";
-import createSideToolbarPlugin from "draft-js-side-toolbar-plugin";
+import React, { Component } from 'react';
 
-import "./draft.css";
-import "draft-js/dist/Draft.css";
-import "draftail/dist/draftail.css";
-import "draft-js-inline-toolbar-plugin/lib/plugin.css";
-import "draft-js-side-toolbar-plugin/lib/plugin.css";
+import Editor, { createEditorStateWithText } from '@draft-js-plugins/editor';
 
-const inlineToolbarPlugin = createInlineToolbarPlugin();
-const { InlineToolbar } = inlineToolbarPlugin;
+import createInlineToolbarPlugin, {
+    Separator,
+} from '@draft-js-plugins/inline-toolbar';
+import {
+    ItalicButton,
+    BoldButton,
+    UnderlineButton,
+    CodeButton,
+    HeadlineOneButton,
+    HeadlineTwoButton,
+    HeadlineThreeButton,
+    UnorderedListButton,
+    OrderedListButton,
+    BlockquoteButton,
+    CodeBlockButton,
+} from '@draft-js-plugins/buttons';
 
-const sideToolbarPlugin = createSideToolbarPlugin();
-const { SideToolbar } = sideToolbarPlugin;
-
-const plugins = [inlineToolbarPlugin, sideToolbarPlugin];
-
-class Draftjs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            editorState: EditorState.createEmpty()
-        };
-        this.changeState = this.changeState.bind(this);
-    }
-
-    changeState(state) {
-        this.setState({
-            editorState: state
+class HeadlinesPicker extends Component {
+    componentDidMount() {
+        setTimeout(() => {
+            window.addEventListener('click', this.onWindowClick);
         });
     }
 
+    componentWillUnmount() {
+        window.removeEventListener('click', this.onWindowClick);
+    }
+
+    onWindowClick = () =>
+        // Call `onOverrideContent` again with `undefined`
+        // so the toolbar can show its regular content again.
+        this.props.onOverrideContent(undefined);
+
     render() {
+        const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
         return (
-            <div className="App">
-                <DraftailEditor
-                    editorState={this.state.editorState}
-                    onChange={this.changeState}
-                    placeholder="Tell your story..."
-                    plugins={plugins}
-                />
-                <InlineToolbar />
-                <SideToolbar />
+            <div>
+                {buttons.map((Button, i) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <Button key={i} {...this.props} />
+                ))}
             </div>
         );
     }
 }
 
-export default Draftjs;
+class HeadlinesButton extends Component {
+    // When using a click event inside overridden content, mouse down
+    // events needs to be prevented so the focus stays in the editor
+    // and the toolbar remains visible  onMouseDown = (event) => event.preventDefault()
+    onMouseDown = (event) => event.preventDefault();
+
+    onClick = () =>
+        // A button can call `onOverrideContent` to replace the content
+        // of the toolbar. This can be useful for displaying sub
+        // menus or requesting additional information from the user.
+        this.props.onOverrideContent(HeadlinesPicker);
+
+    render() {
+        return (
+            <div
+                onMouseDown={this.onMouseDown}
+
+            >
+                <button onClick={this.onClick} >
+                    H
+                </button>
+            </div>
+        );
+    }
+}
+
+const inlineToolbarPlugin = createInlineToolbarPlugin();
+const { InlineToolbar } = inlineToolbarPlugin;
+const plugins = [inlineToolbarPlugin];
+const text =
+    'In this editor a toolbar shows up once you select part of the text …';
+
+export default class CustomInlineToolbarEditor extends Component {
+    state = {
+        editorState: createEditorStateWithText(text),
+    };
+
+    componentDidMount() {
+        this.setState({
+            editorState: createEditorStateWithText(text),
+        });
+    }
+
+    onChange = (editorState) => {
+        this.setState({
+            editorState,
+        });
+    };
+
+    focus = () => {
+        this.editor.focus();
+    };
+
+    render() {
+        return (
+            <div  onClick={this.focus}>
+                <Editor
+                    editorKey="CustomInlineToolbarEditor"
+                    editorState={this.state.editorState}
+                    onChange={this.onChange}
+                    plugins={plugins}
+                    ref={(element) => {
+                        this.editor = element;
+                    }}
+                />
+                <InlineToolbar>
+                    {
+                        // may be use React.Fragment instead of div to improve perfomance after React 16
+                        (externalProps) => (
+                            <div>
+                                <BoldButton {...externalProps} />
+                                <ItalicButton {...externalProps} />
+                                <UnderlineButton {...externalProps} />
+                                <CodeButton {...externalProps} />
+                                <Separator {...externalProps} />
+                                <HeadlinesButton {...externalProps} />
+                                <UnorderedListButton {...externalProps} />
+                                <OrderedListButton {...externalProps} />
+                                <BlockquoteButton {...externalProps} />
+                                <CodeBlockButton {...externalProps} />
+                            </div>
+                        )
+                    }
+                </InlineToolbar>
+            </div>
+        );
+    }
+}
