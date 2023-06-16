e6b8c31362d8bd459ed5d55bd64252155700306a
bipplus1 <markx195>
2023-06-16 11:48:25 +0700

    login

--- src/Login/login.js
+++ src/Login/login.js
@@ -5,7 +5,6 @@ import "./login.css"
 import ImageSlider from "./imageSlider"
 
 const LoginForm = () => {
-    const [token, setToken] = useState("");
     const navigate = useNavigate();
     const slides = [
         {url: "/Images/pic1.png", title: "Pic1"},
@@ -22,32 +21,29 @@ const LoginForm = () => {
         window.location.href = 'https://binote-api.biplus.com.vn/auth/login/google?redirect=https://localhost:3000/';
     }
 
-    async function refreshTokens() {
-        try {
-            const response = await fetch('https://binote-api.biplus.com.vn/auth/refresh', {
-                method: 'POST',
-                credentials: 'include'
-            });
-
-            const data = await response.json();
-            setToken(data.data.access_token);
-
-            if (token) {
-                navigate('/HomePage');
-            }
-        } catch (error) {
-            console.error('Token refresh failed', error);
-        }
+    function refreshTokens() {
+        return fetch('https://binote-api.biplus.com.vn/auth/refresh', {
+            method: 'POST',
+            credentials: 'include'
+        });
     }
 
-// Check if the user has been redirected back from Google login
-    const urlParams = new URLSearchParams(window.location.search);
-    const reason = urlParams.get('reason');
-
-    if (reason !== 'INVALID_PROVIDER') {
-        refreshTokens();
+    function handleLoginAndRefreshTokens() {
+        handleLogin(); // Call handleLogin function
+        setTimeout(() => {
+            refreshTokens()
+                .then(response => response.json())
+                .then(data => {
+                    const accessToken = data.data.access_token;
+                    localStorage.setItem('accessToken', accessToken);
+                    localStorage.setItem('loggedIn', true);
+                    navigate("/HomePage");
+                })
+                .catch((error) => {
+                    console.error('Token refresh failed', error);
+                });
+        }, 1000); // Adjust the delay time as needed
     }
-
 
     return (
         <>
@@ -59,7 +55,10 @@ const LoginForm = () => {
 
                 <div className="login-page">
                     <div className="form rounded-2xl">
-                        <button onClick={handleLogin}>haha</button>
+                        <button onClick={handleLoginAndRefreshTokens} className="flex justify-evenly">
+                            <img src="/Images/googleIcon.svg" alt="Google icon"/>
+                            <div>Đăng nhập với Google</div>
+                        </button>
                     </div>
                 </div>
             </div>
